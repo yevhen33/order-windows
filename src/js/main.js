5b2f8c053601401601f74312f35d03d5d942d2cd
Yevhen <yevhen.code@gmail.com>
2020-08-13 14:43:28 +0300

    собрали все данные от пользователя в формах для расчета

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,14 @@ import './slider';
 import modals from './modules/modal';
 import tabs from './modules/tabs';
 import forms from './modules/forms';
+import changeModalState from './modules/changeModalState';
 
 window.addEventListener('DOMContentLoaded', () => {
+    "use strict";
+
+    let modalState = {};
+
+    changeModalState(modalState);
     modals();
     tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
